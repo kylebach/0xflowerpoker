7997c9d6312d2f92a876165f92ccc84d186aa4c7
Kyle Bachinski <kyle@bachin.ski>
2022-06-19 13:39:06 -0700

    Added Dockerfile and Jenkinsfile

--- secrets.ts
+++ secrets.ts
@@ -2,5 +2,7 @@ require('dotenv').config();
 
 export default {
   rpc: process.env.RPC || 'https://polygon-rpc.com',
-  account: process.env.ACCOUNT || '',
+  account: process.env.ACCOUNT ||
+   '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f',
+  // ^ public private key, dont try using
 };
